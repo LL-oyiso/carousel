17553943b7f2962ddfaf064325b254b32db84109
LL-oyiso <theo.tose.tt@gmail.com>
2023-11-12 20:48:10 +0200

    changes

--- carousel/src/App.jsx
+++ carousel/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const slides = [
+    {url: 'https://images.unsplash.com/photo-1461849038508-25d35f33c344?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHVibGljJTIwdHJhbnNwb3J0YXRpb258ZW58MHwwfDB8fHww'},
+    {url: 'https://images.unsplash.com/photo-1598637419436-7e96a4488ed0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cHVibGljJTIwdHJhbnNwb3J0YXRpb258ZW58MHwwfDB8fHww'},
+    {url: 'https://images.unsplash.com/photo-1543337280-0a85de23072f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHVibGljJTIwdHJhbnNwb3J0YXRpb258ZW58MHwwfDB8fHww'},
+    {url: 'https://images.unsplash.com/photo-1559835557-7766a856e6ee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHVibGljJTIwdHJhbnNwb3J0YXRpb258ZW58MHwwfDB8fHww'},
+    {url: 'https://images.unsplash.com/photo-1611697930219-e76441073035?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHB1YmxpYyUyMHRyYW5zcG9ydGF0aW9ufGVufDB8MHwwfHx8MA%3D%3D'},
+  ];
 
   return (
-    <>
+   
+     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative '>
+      <div style={{backgroundImage: `url(${slides[0].url})` }} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'></div>
+      {/* {left arrow} */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <BsChevronCompactLeft  size={30} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      {/* {right arrow } */}
+      <div>
+      <BsChevronCompactRight size={30} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     </div>
+   
   )
 }
 
